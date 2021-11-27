e0e0bfbf575575d126c412fc3e0b6aa441e07fae
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-27 20:33:56 +0900

    firebaseの設定

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEYREACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_DATABASE,
+  storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+export const storage = firebase.storage()
+export const provider =  new firebase.auth.GoogleAuthProvider()
