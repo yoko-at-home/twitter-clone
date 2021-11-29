e2193b04454cd6ccb8893e69a6ea13b75f680548
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-29 16:35:16 +0900

    login/logoutロジックの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+import Feed from "./component/Feed";
 
 const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
 };
 
 export default App;
