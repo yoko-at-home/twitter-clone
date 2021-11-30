09b71299aba723f6c0a9a7bdf3a8580bd87d08fd
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-30 14:39:47 +0900

    仮サインアウトボタン

--- src/component/Feed.tsx
+++ src/component/Feed.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { auth } from "../firebase";
 
 const Feed = () => {
-  return <div></div>;
+  return (
+    <div>
+      Feed
+      <button onClick={() => auth.signOut()}>ログアウト</button>
+    </div>
+  );
 };
 
 export default Feed;
