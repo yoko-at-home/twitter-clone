51679aa415aaf447179ad01024c2c86c66017998
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-30 14:34:36 +0900

    bugの修正

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,8 +6,8 @@ import "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_DATABASE,
-  storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
